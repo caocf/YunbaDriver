38f0fd4e1dd714f1b8a6fd51dddcfe88cda1edc8
momosibyl <whyxlm@126.com>
2015-11-24 22:56:01 +0800

    Initial Commit

---
+++ YunbaDriver/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  YunbaDriver
+//
+//  Created by momosibyl on 15/11/24.
+//  Copyright © 2015年 maiding. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
